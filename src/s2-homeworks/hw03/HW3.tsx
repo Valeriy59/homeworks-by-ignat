9e4f79dabf8e25c03805579ca082edbd2234cf28
Valeriy Litvinov <valitvinoff@mail.ru>
2022-10-10 10:36:51 +0300

    HW4

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { v1 } from 'uuid'
+import React, {useState} from 'react'
+import {v1} from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
@@ -24,7 +24,8 @@ export type UserType = {
 }
 
 export const pureAddUserCallback = (name: string, setUsers: (u: UserType[]) => void, users: UserType[]) => { // need to fix any
-    const user = { _id: v1(), name // need to fix
+    const user = {
+        _id: v1(), name // need to fix
     }
     setUsers([...users, user])
 }
@@ -41,7 +42,7 @@ const HW3 = () => {
         <div id={'hw3'}>
             <div className={s2.hwTitle}>Homework #3</div>
             {/*для автоматической проверки дз (не менять)*/}
-
+            <hr/>
             <div className={s2.hw}>
                 <GreetingContainer
                     users={users}
