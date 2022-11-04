4e8d1e3442a7a931db5dcba325ebcbf26e8dcff2
Valeriy Litvinov <valitvinoff@mail.ru>
2022-11-04 11:42:15 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -11,7 +11,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             return state.sort((u1,u2) => action.payload === 'up' ? u1.name.localeCompare(u2.name): u2.name.localeCompare(u1.name)) // need to fix
         }
         case 'check': {
-
+            action.payload = 18
             return state.filter(u => u.age >= action.payload) // need to fix
         }
         default:
