693c6c584e70b53fed53981464b28ae3ddb84fb4
Valeriy Litvinov <valitvinoff@mail.ru>
2022-11-04 11:44:26 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,7 +8,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
 
-            return state.sort((u1,u2) => action.payload === 'up' ? u1.name.localeCompare(u2.name): u2.name.localeCompare(u1.name)) // need to fix
+            return [...state.sort((u1,u2) => action.payload === 'up' ? u1.name.localeCompare(u2.name): u2.name.localeCompare(u1.name))] // need to fix
         }
         case 'check': {
             action.payload = 18
