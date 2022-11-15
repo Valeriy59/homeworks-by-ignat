75ae2dea44fc8cfbbb10ddedaa0f6d1cac723c80
Valeriy Litvinov <valitvinoff@mail.ru>
2022-11-15 22:53:13 +0300

    HW10

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,24 @@ import './index.css'
 // import App from './p1-main/m1-ui/u1-app/App'
 import App from "./s1-main/App";
 import * as serviceWorker from './serviceWorker'
+import {Provider} from "react-redux";
+import store from "./s2-homeworks/hw10/bll/store";
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App/>
+//     </React.StrictMode>,
+//     document.getElementById("root")
+// )
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 )
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
